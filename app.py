be5b869d7fab512192f9b29240925834c6ec9f8a
undefspi <chris.beech@wavelytics.co.uk>
2018-07-09 22:36:57 +0100

    change of message to highlight difference

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome to the docker version of the openshift test!"
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return 'I am building images from a docker file and into openshift?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
